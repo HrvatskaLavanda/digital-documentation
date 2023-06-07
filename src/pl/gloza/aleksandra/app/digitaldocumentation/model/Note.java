76e691944a57fd2789a6587a7e3970adb2badc58
Aleksandra Gloza <aleksandra.gloza@gmail.com>
2023-06-07 17:39:41 +0200

    added models

---
+++ src/pl/gloza/aleksandra/app/digitaldocumentation/model/Note.java
@@ -0,0 +1,18 @@
+package pl.gloza.aleksandra.app.digitaldocumentation.model;
+
+import java.time.LocalDate;
+
+public class Note {
+    private String name;
+    private String description;
+    private LocalDate created;
+    private byte[] content;
+
+    public void addNotes() {
+
+    }
+
+    public void loadPaperNotes() {
+
+    }
+}
